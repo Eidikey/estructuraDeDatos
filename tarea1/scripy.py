f8dcb17002cc38cfcfd01032a93b6c34707e4466
Eidikey <alexneithor@gmail.com>
2026-08-20 22:56:12 -0600

    update: primera parte

--- tarea1/scripy.py
+++ tarea1/scripy.py
@@ -1,1 +1,15 @@
+import csv
 
+with open ('datos_redes_sociales.csv', mode ='r', encoding='utf-8') as archivo:
+    lector = csv.reader(archivo, delimiter=',')
+
+    encabezados = next(lector)
+    print(encabezados)
+
+    for fila in lector:
+        print(fila)
+
+    datos = list(lector)
+    datoEspecifico = datos[2][2]
+
+    print(datoEspecifico)
