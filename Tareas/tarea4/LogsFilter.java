7381d10923b32c39623820861696ab2d72f9586a
Eidikei <alexneithor@gmail.com>
2026-09-12 21:03:20 -0600

    Tarea4 completada

--- Tareas/tarea4/LogsFilter.java
+++ Tareas/tarea4/LogsFilter.java
@@ -5,17 +5,26 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class LogsFilter {
-    public static void main(String[] args) {
-        ConjuntoADT<String> ipsUnicas = new ConjuntoADT<>();
-        
-        try {
-            List<String> lineas = Files.readAllLines(Paths.get("access.log"), StandardCharsets.UTF_8);
-            
-            for (int i = 0; i < args.length; i++) {
-              
-            }
-        } catch (IOException e) {
-            System.err.println("Error al leer el archivo de logs.");
+  public static void main(String[] args) {
+    ConjuntoADT<String> ipsUnicas = new ConjuntoADT<>();
+
+    try {
+      List<String> lineas = Files.readAllLines(Paths.get("access.log"), StandardCharsets.UTF_8);
+      for (String linea : lineas) {
+        if (!linea.trim().isEmpty()) {
+          String ip = linea.split(" ")[0];
+          ipsUnicas.agregarElemento(ip);
         }
+      }
+
+      System.out.println("--- Filtrado de logs completo ---");
+      System.out.println("Total de lineas leidas: " + lineas.size());
+      System.out.println("Cantidad de visitantes UNICOS: " + ipsUnicas.longitud());
+      System.out.println("\nLista de IPs únicas encontradas:");
+      System.out.println(ipsUnicas);
+
+    } catch (IOException e) {
+      System.err.println("Error al leer el archivo de logs.");
     }
+  }
 }
